f86786b1906d29035b771ed0a325e729044c3187
cynicalfox47 <cynicalfox47@gmail.com>
2019-08-03 18:44:23 +0800

    Removed unnecessary lines.

--- mcbot.js
+++ mcbot.js
@@ -7,37 +7,29 @@ var statustring = "No signal";
 var request = require('request');
 var mcCommand = '/minecraft'; // Command for triggering
 var mcIP = settings.ip; // Your MC server IP
-var mcPort = settings.port; // Your MC server port
 
-var url = 'http://mcapi.us/server/status?ip=' + mcIP + '&port=' + mcPort;
+var url = 'https://api.mcsrvstat.us/2/' + mcIP;
 
 
 function update() {
-  /*seconds = seconds + 1;
-  secondsString = seconds.toString();
-  client.user.setActivity(secondsString, { type: 'Playing' })
-  .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : 'none'}`))
-  .catch(console.error);*/
-  request(url, function(err, response, body) {
+    request(url, function(err, response, body) {
       if(err) {
           console.log(err);
           //return message.reply('Error getting Minecraft server status...');
       }
+
       body = JSON.parse(body);
       var status = 'Server offline';
       console.log(body.motd);
+
       if(body.online) {
-          if((body.motd=="&cWe are under maintenance.")||(body.players.now>=body.players.max)){
-            client.user.setStatus('idle')
-            //.then(console.log)
-            .catch(console.error);
-          }else{
+            //set status to online if True
             client.user.setStatus('online')
             //.then(console.log)
             .catch(console.error);
-          }
-            if(body.players.now) {
-                status = ' ' + body.players.now + '  of  ' + body.players.max;
+
+            if(body.players.online) {
+                status = ' ' + body.players.online + '  of  ' + body.players.max;
               } else {
                 status = ' 0  of  ' + body.players.max;
         }
@@ -57,14 +49,5 @@ client.on("ready", () => {
   console.log("I am ready!");
   client.setInterval(update,30000);
 });
-
-/*client.on("message", (message) => {
-  if (message.content.startsWith("ping")) {
-    message.channel.send("pong!");
-    update();
-
-  }
-}
-);*/
 
 client.login(settings.token);
